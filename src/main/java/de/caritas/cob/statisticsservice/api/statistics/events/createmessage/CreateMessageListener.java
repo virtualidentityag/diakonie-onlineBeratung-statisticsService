d9453452a19ec643075f41d919d53bd858eb2f39
Daniel Hillwig <hillwig@open4business.de>
2021-09-20 16:20:16 +0200

    chore: optimization

--- src/main/java/de/caritas/cob/statisticsservice/api/statistics/events/createmessage/CreateMessageListener.java
+++ src/main/java/de/caritas/cob/statisticsservice/api/statistics/events/createmessage/CreateMessageListener.java
@@ -47,6 +47,9 @@ public class CreateMessageListener {
   }
 
   private CreateMessageMetaData buildMetaData(CreateMessageStatisticsEventMessage eventMessage) {
-    return CreateMessageMetaData.builder().hasAttachment(eventMessage.getHasAttachment()).build();
+    return CreateMessageMetaData
+        .builder()
+        .hasAttachment(eventMessage.getHasAttachment())
+        .build();
   }
 }
