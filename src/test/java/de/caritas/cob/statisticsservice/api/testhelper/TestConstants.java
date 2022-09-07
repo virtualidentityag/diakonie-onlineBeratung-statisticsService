a51e689655938ffd78bec70d3348c8f8a4bee89f
PhilippFruh <philipp.fruh@pm.me>
2022-09-07 13:19:06 +0200

    test: RegistrationListenerTest

--- src/test/java/de/caritas/cob/statisticsservice/api/testhelper/TestConstants.java
+++ src/test/java/de/caritas/cob/statisticsservice/api/testhelper/TestConstants.java
@@ -12,6 +12,7 @@ public class TestConstants {
   public static final String RC_GROUP_ID = "yhT3444kk";
   public static final Long SESSION_ID = 34344L;
   public static final String CONSULTANT_ID = "d63f4cc0-215d-40e2-a866-2d3e910f0590";
+  public static final String ASKER_ID = "d63f4cc0-215d-40e2-a866-2d3e910f0591";
   public static final Long AGENCY_ID = 23447L;
   public static final int CONSULTING_TYPE_ID = 1;
   public static final String VIDEO_CALL_UUID = "123e4567-e89b-12d3-a456-556642440000";
