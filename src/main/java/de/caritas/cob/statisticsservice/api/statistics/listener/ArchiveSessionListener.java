86a3d213d32ce4bffa8001e8c59de1266146064c
Patric Dosch <patric.dosch@virtual-identity.com>
2022-10-14 11:45:45 +0200

    feat: VIC-1342 - Data Export - End date field is populated with the date when chat was Archived
    - fix Checkstyle violation (even the google formatter format it like that)

--- src/main/java/de/caritas/cob/statisticsservice/api/statistics/listener/ArchiveSessionListener.java
+++ src/main/java/de/caritas/cob/statisticsservice/api/statistics/listener/ArchiveSessionListener.java
@@ -34,8 +34,7 @@ public class ArchiveSessionListener {
 
     StatisticsEvent statisticsEvent =
         StatisticsEventBuilder.getInstance(
-                () ->
-                    userStatisticsService.retrieveSessionViaSessionId(eventMessage.getSessionId()))
+            () -> userStatisticsService.retrieveSessionViaSessionId(eventMessage.getSessionId()))
             .withEventType(eventMessage.getEventType())
             .withTimestamp(eventMessage.getTimestamp().toInstant())
             .withUserId(eventMessage.getUserId())
