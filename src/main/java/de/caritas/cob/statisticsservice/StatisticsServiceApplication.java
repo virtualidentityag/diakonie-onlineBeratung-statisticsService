966ddfd3cdbc818e9820814dad1964a6837bbae3
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-04-18 15:56:00 +0200

    fix: add healthcheck

--- src/main/java/de/caritas/cob/statisticsservice/StatisticsServiceApplication.java
+++ src/main/java/de/caritas/cob/statisticsservice/StatisticsServiceApplication.java
@@ -3,8 +3,9 @@ package de.caritas.cob.statisticsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, EmbeddedMongoAutoConfiguration.class})
 public class StatisticsServiceApplication {
 
   public static void main(String[] args) {
