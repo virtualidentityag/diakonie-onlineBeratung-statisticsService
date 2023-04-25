4d9ef1d2b82c08a85e568856aa6819e9339fe890
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-04-25 10:24:39 +0200

    fix: add healthcheck, removed actuator due to securityFilterChain vs KeycloakWebSecurityConfigurerAdapter incompatibility

--- src/main/java/de/caritas/cob/statisticsservice/StatisticsServiceApplication.java
+++ src/main/java/de/caritas/cob/statisticsservice/StatisticsServiceApplication.java
@@ -3,14 +3,9 @@ package de.caritas.cob.statisticsservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, EmbeddedMongoAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class StatisticsServiceApplication {
-
-  static {
-    System.setProperty("os.arch", "i686_64");
-  }
 
   public static void main(String[] args) {
     SpringApplication.run(StatisticsServiceApplication.class, args);
